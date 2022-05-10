6ed5ddd39c5473f724de882ac343046e5a0272b3
WinLeiTheingi <105236669+WinLeiTheingi@users.noreply.github.com>
2022-05-10 17:09:52 +0630

    Add files via upload

--- CreateFile.java
+++ CreateFile.java
@@ -1,50 +1,66 @@
 package forJavaTest;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.io.IOException;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class CreateFile {
     
     int x = 0;
     File newFile;
-    Scanner sc;
+    private static final long MEGABYTE = 1024L * 1024L;
     
     // Constructor
     public CreateFile() {
         System.out.println("Welcome from my program!" + "\r\n");
-        
-        // Create a Scanner object to read user input
-        sc = new Scanner(System.in);
     }
     
-    // user input 
-    public void getUserInput() {
+	/*
+	 * user input
+	 * if valid number, return true.
+	 * if invalid number, return false.
+	 * return "false" will prompt again user input. 
+	 */
+    public boolean getUserInput() {
         // Create a Scanner object to read user input
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter Number of lines from 1 to 230 to write in file : ");
+        System.out.println("Enter Number of lines from 1 to 299 to write in file : ");
         
-        try {
-            // Read user input
-            x = sc.nextInt();
-        } catch(InputMismatchException e) {
-            System.out.println("Please enter only number.");
-        }
-        
-        //if catch exception, prompt again user input
-        // todo
+		try {
+			// Read user input
+			x = sc.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("Please enter only number." + "\r\n");
+			return false;
+		}
         
         // Check Number Range
-        // todo, checkNumber();
+		if (!checkNumber(x)) {;
+			return false;
+		}
         
         if (x > 1) {
             System.out.println(x + " lines will write in file." + "\r\n");
         } else if (x == 1) {
             System.out.println(x + " line will write in file." + "\r\n");
         }
+        return true;
+    }
+    
+    // Check Number Range
+    public boolean checkNumber(int num) {
+    	
+    	//check whether number is between 1 to 299 or not
+    	if (num >= 1 && num <= 299) {
+    		return true;
+    	} else {
+    		System.out.println("Invalid. Number of lines must be from 1 to 299!" + "\r\n");
+        	return false;
+    	}
     }
     
     // output File
@@ -58,36 +74,34 @@ public class CreateFile {
         		System.out.println("File created in \"" + newFile + "\"" + "\r\n");
         		return true;
         	} else {
-        		System.out.println("File already exists.");
-        		
-        		// ask to override or not
+        		// if file exists, ask to override or not
         		// if yes, deleteFile, createNewFile and return true.
         		// if no, do nothing and return false.
-//        		System.out.println("File already exists. Do you want to override file?(y/n)");
-//        		String strReply = sc.nextLine();
-//        		if (strReply == "y") {
-//        			newFile.delete();
-//        			
-//        			// call itself to create new file
-//        			outputFile();
-//        		} else if (strReply == "n") {
-//        			return false;
-//        		} else {
-//        			System.out.println("Please enter only y or n.");
-//        			
-//        			// request user input again. todo
-//        		}
-        		return false;
+				System.out.println(
+						"File already exists in \"" + newFile + "\"" + "\r\n" + "Do you want to override file?(y/n)");
+  		
+                // Create a Scanner object to read user input
+                Scanner sc = new Scanner(System.in);
+
+                String strReply = sc.nextLine();
+        		if (strReply.equals("y")) {
+        			newFile.delete();
+        		} else if (strReply.equals("n")) {
+        			System.out.println("Program ends. Please change your file name and start again!" + "\r\n");
+        			return false;
+        		}
+        		else {
+        			System.out.println("Please enter only y or n." + "\r\n");
+        		}
+        		// call itself to create new file
+    			outputFile();
+        		return true;
         	}
 		} catch (IOException e) {
 			System.out.println("An error occurred in output file.");
 			e.printStackTrace();
 			return false;
-		} catch (InputMismatchException e) {
-			System.out.println("Please enter only y or n.");
-			return false;
-        }
-		
+		}
     }
     
     // write file
@@ -117,11 +131,7 @@ public class CreateFile {
             }
             myWriter.close();
             
-            System.out.println("Successfully wrote to the file.");
-            
-            // print number of characters in one line. ToDo
-            //int noChars = Integer.parseInt(strFront) + Integer.parseInt(strEnd);
-            //System.out.println("Number of characters in one line : " + Integer.toString(noChars));
+            System.out.println("Successfully wrote to the file." + "\r\n");
             
           } catch (IOException e) {
             System.out.println("An error occurred in write file.");
@@ -129,7 +139,22 @@ public class CreateFile {
           }
     }
     
-    private static final long MEGABYTE = 1024L * 1024L;
+    // read file
+    public void readFile() {
+        // read file and print number of characters in one line. (Read only first line)
+    	try {
+    	      Scanner myReader = new Scanner(newFile);
+    	      
+    	      if (myReader.hasNextLine()) {
+    	        String data = myReader.nextLine();
+    	        System.out.println("Number of characters in one line : " + data.length() + "\r\n");
+    	      }
+    	      myReader.close();
+    	    } catch (FileNotFoundException e) {
+    	      System.out.println("An error occurred in read file.");
+    	      e.printStackTrace();
+    	    }
+    }
 
     public static long bytesToMegabytes(long bytes) {
         return bytes / MEGABYTE;
@@ -141,20 +166,24 @@ public class CreateFile {
     	
         // call constructor
         CreateFile crtfile = new CreateFile();
-        
-        // call getUserInput
-        crtfile.getUserInput();
+    	
+        // call getUserInput. loop until right input
+        while (!crtfile.getUserInput()) {
+        	crtfile.getUserInput();
+        }
         
         // call outputFile
         if (crtfile.outputFile()) {
         	// call writeFile
             crtfile.writeFile();
+            
+            // call readFile
+            crtfile.readFile();
         };
         
         long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         long actualMemUsed = afterUsedMem - beforeUsedMem;
         
-        //System.out.println("beforeUsedMem in megabytes : " + bytesToMegabytes(beforeUsedMem) + ", afterUsedMem in megabytes : " + bytesToMegabytes(afterUsedMem));
-        System.out.println("Used memory in megabytes : " + bytesToMegabytes(actualMemUsed) + "MB");
+        System.out.println("Program Used memory in megabytes : " + bytesToMegabytes(actualMemUsed) + "MB");
     }
 }
